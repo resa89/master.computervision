39ddd188255dd28ff0e7f56192eacaafbadb303c
Thorsten Niehues <thorsten.niehues@gmail.com>
2014-04-03 18:04:13 +0200

    use biliniar interpolation

--- ex1.m
+++ ex1.m
@@ -15,8 +15,19 @@ for i=1:m*3
         tmp = [i-m j-n 0] * transform;
         iOld = round(tmp(1));
         jOld = round(tmp(2));
-        if iOld <= m && iOld > 0 && jOld <=n && jOld > 0
-            newM(i, j) = gray(iOld, jOld);
+        
+        yx0=floor(tmp);
+        yx1=ceil(tmp);
+        
+        if yx1(1) <= m && yx0(1) > 0 && yx1(2) <=n && yx0(2) > 0
+            wt = tmp - yx0; 
+            wtConj = 1 - wt;
+            
+            interTop = wtConj(2) * gray(yx0(1),yx0(2)) + wt(2) * gray(yx0(1),yx1(2));
+            interBtm = wtConj(2) * gray(yx1(1),yx0(2)) + wt(2) * gray(yx1(1),yx1(2));
+            interVal=wtConj(1) * interTop + wt(1) * interBtm;
+            
+            newM(i, j) = interVal;
         end
     end
 end
