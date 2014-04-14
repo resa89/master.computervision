4696e31fc67b5ec9f3d92d408a5acb643f82aee6
Thorsten Niehues <thorsten.niehues@gmail.com>
2014-04-14 12:35:48 +0200

    added new files, allow next neighbor

---
+++ transfrom.m
@@ -0,0 +1,39 @@
+function outputPic = transfrom(A, gray, neighbor)
+    [m,n] = size(gray);
+    newM = repmat(uint8(0),m*3, n*3);
+    
+    for i=1:m*3
+        for j=1:n*3
+            tmp = [i-m j-n 0] * A;
+            yx0=floor(tmp);
+            yx1=ceil(tmp);
+
+            if yx1(1) <= m && yx0(1) > 0 && yx1(2) <=n && yx0(2) > 0
+                wt = tmp - yx0; 
+                wtConj = 1 - wt;
+
+                if neighbor
+                    if wt(1) >= 0.5
+                        y=yx1(1);
+                    else
+                        y=yx0(1);
+                    end
+                    if wt(2) >= 0.5
+                        x = yx1(2);
+                    else 
+                        x=yx0(2);
+                    end
+                    interVal = gray(y,x);
+                
+                else
+                    interTop = wtConj(2) * gray(yx0(1),yx0(2)) + wt(2) * gray(yx0(1),yx1(2));
+                    interBtm = wtConj(2) * gray(yx1(1),yx0(2)) + wt(2) * gray(yx1(1),yx1(2));
+                    interVal=wtConj(1) * interTop + wt(1) * interBtm;
+                end
+                newM(i, j) = interVal;
+            end
+        end
+    end
+    outputPic = newM;
+end
+
