050d3f2682b948dbd4c33586ea744782b7261e0f
Thorsten Niehues <thorsten.niehues@gmail.com>
2014-04-27 15:49:38 +0200

    rotate around center

--- transfrom.m
+++ transfrom.m
@@ -3,9 +3,14 @@ function outputPic = transfrom(A, gray, a0, neighbor, newM)
     %newM = zeros(m*3, n*3, o);
     [m,n,o] = size(newM);
     
+    m2 = m/2;
+    n2 = n/2;
+    
     for i=1:m
         for j=1:n
-            tmp = [i j] * A + a0;
+            tmp = [i-m2 j-n2] * A + a0;
+            tmp(1) = tmp(1) + m2;
+            tmp(2) = tmp(2) + n2;
             yx0=floor(tmp);
             yx1=ceil(tmp);
 
