1bee65f2a589badcd819e77a8614568799398f86
Thorsten Niehues <thorsten.niehues@gmail.com>
2014-05-16 13:12:22 +0200

    bug-fix: use all 4 passpoints

--- ex3/ex3.m
+++ ex3/ex3.m
@@ -8,8 +8,8 @@ img = im2double(img);
 dx = 381;
 dy = 108;
 
-x1 = [10.5 12.5 224 223.5] * 100;
-y1 = [0 77];
+x1 = [10.5 12.5 224 223.5] * 10;
+y1 = [0 77 0 77] * 10;
 
 % Pixels for 1st picture
 scale2 = 1;
@@ -30,7 +30,7 @@ yp1 = [443 1443  404 1425] / scale2;
 % xp1 = [ 478  529 1258 1258 ];
 % yp1 = [1194 2247 1199 2247 ];
 
-newM = rectification([x1(1) x1(1) x1(2) x1(2)], xp1, [y1(1) y1(2) y1(1) y1(2)], yp1, img);
+newM = rectification(x1, xp1, y1, yp1, img);
 
 imshow(newM);   % Bild darstellen
 % imshow(img);
