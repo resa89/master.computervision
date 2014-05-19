e8acbe5823702f51a0dd94ae0911f090af95acc5
Thorsten Niehues <thorsten.niehues@gmail.com>
2014-05-19 11:17:09 +0200

    two img in one big matrix

--- ex3/ex3.m
+++ ex3/ex3.m
@@ -6,7 +6,7 @@ img = im2double(img);
 img2 = im2double(img2);
 
 x1 = [10.5 12.5 224 223.5] * 10;
-x2 = [224 223.5 347.5 347.5] - 123.5;
+x2 = [224 223.5 347.5 347.5];
 x2 = x2 * 10;
 y = [20 97 20 97] * 10;
 
@@ -28,9 +28,10 @@ yp2 = [ 451 1447  439 1487];
 % xp1 = [ 478  529 1258 1258 ];
 % yp1 = [1194 2247 1199 2247 ];
 
-% newM = rectification(x1, xp1, y, yp1, img);
-newM2 = rectification(x2, xp2, y, yp2, img2);
+newM = zeros(3000, 14000, 3);
+newM = rectification2(x1, xp1, y, yp1, img, newM, 1, 1);
+newM = rectification2(x2, xp2, y, yp2, img2, newM, 1, 1);
 
-% imshow(newM);   % Bild darstellen
-imshow(newM2);   % Bild darstellen
+imshow(newM);   % Bild darstellen
+% imshow(newM2);   % Bild darstellen
 % imshow(img2);
