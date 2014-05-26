c9407729f52897c638db52daf922fa631da49866
Thorsten Niehues <thorsten.niehues@gmail.com>
2014-05-26 16:36:42 +0200

    Finished ex4 task 1a

---
+++ ex4/ex4.m
@@ -0,0 +1,4 @@
+m = matfile('robot-corridor.mat')
+% n = matfile('signals.mat')
+
+show_seq(m.seq);
---
+++ ex4/show_seq.m
@@ -0,0 +1,11 @@
+function show_seq(seq)
+    [t,y,x] = size(seq);
+    
+    for num=1 : t
+        img = seq(num,:,:);
+        img = squeeze(img);
+%         imshow(img);
+        show_img(img, [0 1], 'auto', 'image sequence', 256);
+        waitforbuttonpress;
+    end
+end
