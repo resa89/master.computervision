f867647c43410267f862f2f570e91d31a8e8f06b
Thorsten Niehues <thorsten.niehues@gmail.com>
2014-05-27 11:20:50 +0200

    ex4 1b is working (not 100% done yet)

--- ex4/ex4.m
+++ ex4/ex4.m
@@ -1,4 +1,7 @@
-m = matfile('robot-corridor.mat')
+% m = matfile('robot-corridor.mat')
+% show_seq(m.seq);
+
+make_seq(10, 10, 50, 2);
 % n = matfile('signals.mat')
 
-show_seq(m.seq);
+
---
+++ ex4/make_seq.m
@@ -0,0 +1,27 @@
+function make_seq(dimt, dimy, dimx, v)
+    % dimt 
+    
+    % wavelength lamda = 0.5 * dimx 
+    % sin(0)=0 sin(90)=1, sin(180)=0 sin(270)=-1 sin(360)=0 ...
+    % => dimx ~ 360°
+    
+    % dimy height of the sinus => dimy * sin()
+    
+    % v pixel per timeslot
+    
+    M = zeros(dimx, dimx);
+    
+    xScale = 4 * pi / dimx;
+    for t=0:dimt
+        offset = t * v;
+        for x=0:dimx
+            fx = sin(xScale * (x + offset)); % fx [-1 1];
+%             y = fx * dimy;
+            M(:,x+1) = fx;
+        end
+        imshow(M);
+        show_img(M, [-dimy dimy], 'auto', 'image sequence', 256);
+        waitforbuttonpress;
+    end
+end
+
